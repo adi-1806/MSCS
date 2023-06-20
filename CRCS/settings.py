821f52039765f6cefa9f11fbbe954a6ddc447f17
Peddi Adithya vardhan <peddiadithyavardhan@Peddis-MacBook-Pro.local>
2023-06-20 14:26:29 +0530

    time

--- CRCS/settings.py
+++ CRCS/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hk47a8*b0csm#ju-*=03-chkp2!$r^e2&%#v7^!nqoya65075&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
